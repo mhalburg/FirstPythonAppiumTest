464e68a0cb33c59ee78c251aa9893b6c991e0901
delegate <mvhdbl@gmail.com>
2022-08-05 16:05:50 +0530

    test 2

--- AppiumTest.py
+++ AppiumTest.py
@@ -3,23 +3,31 @@ import unittest
 
 from appium import webdriver
 
+import warnings
+
+
+
 class AppiumTest(unittest.TestCase):
     option = {}
     driver = None
 
     def setUp(self):
-        self.option['app'] = os.getcwd()+"/eribank.apk"
-        self.option['appPackage'] = "com.experitest.ExperiBank"
-        self.option['appActivity'] = ".LoginActivity"
-        self.option['platformName'] = 'Android'
-        self.option['deviceName'] = 'emulator-5554'
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub",self.option)
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=DeprecationWarning)
+            self.option['app'] = os.getcwd()+"/eribank.apk"
+            self.option['appPackage'] = "com.experitest.ExperiBank"
+            self.option['appActivity'] = ".LoginActivity"
+            self.option['platformName'] = 'Android'
+            self.option['deviceName'] = 'emulator-5554'
+            self.driver = webdriver.Remote("http://localhost:4723/wd/hub",self.option)
 
     def testFirstAutomationTest(self):
-        print(self.driver.capabilities)
+        print(self.driver.mobile.DATA_NETWORK)
         #if len(self.driver.find_elements("//*[@text='OK']")) > 0: self.driver.find_element( "//*[@text='OK']").click()
         #self.driver.find_element("//*[@text='Username']").send_keys("company")
         #self.driver.find_element("//*[@text='Password']").send_keys("company")
         #self.driver.find_element("//*[@text='Login']").click()
 
     def tearDown(self): self.driver.quit()
+
+
