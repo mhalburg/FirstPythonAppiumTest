ae11b1118371d2fe7297c23368218d977e10b3af
mhalburg <mvhdbl@gmail.com>
2022-08-05 14:18:29 +0530

    Initial commit

---
+++ AppiumTest.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from appium import webdriver
+
+class AppiumTest(unittest.TestCase):
+    option = {}
+    driver = None
+
+    def setUp(self):
+        self.option['app'] = os.getcwd()+"/eribank.apk"
+        self.option['appPackage'] = "com.experitest.ExperiBank"
+        self.option['appActivity'] = ".LoginActivity"
+        self.option['platformName'] = 'Android'
+        self.option['deviceName'] = 'emulator-5554'
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub",self.option)
+
+    def testFirstAutomationTest(self):
+        if len(self.driver.find_elements("//*[@text='OK']")) > 0: self.driver.find_element( "//*[@text='OK']").click()
+        self.driver.find_element("//*[@text='Username']").send_keys("company")
+        self.driver.find_element("//*[@text='Password']").send_keys("company")
+        self.driver.find_element("//*[@text='Login']").click()
+
+    def tearDown(self): self.driver.quit()
